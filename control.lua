62237ff2fc82d46f8bf9905902dcca4760588d9c
Mission <jackedwardsparrow@gmail.com>
2026-04-13 12:16:13 +0200

    functional medkit

--- control.lua
+++ control.lua
@@ -13,9 +13,22 @@ script.on_load(function()
 end)
 
 script.on_event("on_player_used_capsule", function(event)
-    storage.healing_tick = math.randomm(180, 720)
+    if event.item.name == "medkit" then
+        game.print(event.item.name)
+        storage.healing_tick = math.random(1800, 7200)
+        game.print(storage.healing_tick)
+    end
+    
 end)
 
-script.on_tick(function(event)
-    
+script.on_event("on_tick", function(event)
+    if storage.healing_tick >= -1 then
+        game.print(storage.healing_tick)
+        if storage.healing_tick == 0 then
+            for key, value in pairs(game.players) do
+                value.character.damage(-2000, value.force.name)        
+            end
+        end
+        storage.healing_tick = storage.healing_tick -1
+    end
 end)
